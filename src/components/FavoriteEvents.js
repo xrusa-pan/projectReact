6dc18935ea58a3f51ffae43bd1beb48c8bf47b11
Chrysa Panagiotopoulou <chrysa.nana@gmail.com>
2024-05-30 17:37:04 +0300

    changes

--- src/components/FavoriteEvents.js
+++ src/components/FavoriteEvents.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Row, Col } from 'react-bootstrap';
+import { Card, Button, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const FavoriteEvents = ({ favoriteEvents, removeFromFavorites }) => {
